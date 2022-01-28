cc90f6550762f2711211bb309b5a0485176f2643
Giancarlo Fontela da Luz <giancarlo.luz@cloud2go.com.br>
2022-01-28 16:30:49 -0300

    Changed from Yarn to NPM

--- standalone/src/services/database.js
+++ standalone/src/services/database.js
@@ -5,44 +5,26 @@ const user = process.env.DB_USER || 'postgres';
 const password = process.env.DB_PASSWORD || 'postgres';
 const database = process.env.DB_DATABASE || 'postgres';
 
-module.exports = function ({ autoConnect = false } = {}) {
-    const context = {};
+module.exports = function () {
     const pool = new Pool({ user, port, password, database });
 
-    async function connect() {
-        if (context.client) return;
-        context.client = await pool.connect();
-    }
-
     async function disconnect() {
-        if (!context.client) return;
-        context.client = null;
         await pool.end();
     }
 
     async function query(query) {
-        await ensureClient();
-
-        return (await context.client.query(String(query).trim())).rows;
+        const result = await pool.query(String(query).trim());
+        return result.rows;
     }
 
     function prepare(statement) {
         return async (...params) => {
-            await ensureClient();
-            return (await context.client.query(statement, params)).rows;
+            const result = await pool.query(statement, params);
+            return result.rows;
         };
     }
 
-    async function ensureClient() {
-        if (!context.client) {
-            if (!autoConnect) throw new Error('Not connected to database');
-            
-            await connect();
-        }
-    }
-
     return {
-        connect,
         disconnect,
         query,
         prepare
